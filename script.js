061e74f518ed587fb91b66124b7861a28a8d2a29
AravindSingh1 <142814073+AravindSingh1@users.noreply.github.com>
2024-05-21 11:11:17 +0530

    Update script.js
    Optimised code

--- script.js
+++ script.js
@@ -2,160 +2,73 @@ import * as THREE from 'three';
 import { OrbitControls } from '/OrbitControls.js';
 import { GLTFLoader } from '/GLTFLoader.js';
 
-let selectedFileEl;
-const getBtn = document.querySelectorAll("#getBtn");
-let renderingDiv;
-let classToIdx = { first: 0, second: 1, third: 2 };
-let allModels = [];
-let allCtrrls = [];
-let cameraData = [{ x: -5, y: 2, z: -5 }, { x: -5, y: 2, z: -5 }, { x: -5, y: 2, z: -5 }]
+const fullSvgEl = document.getElementById("parentSVG");
+const selectedFiles = document.getElementById("filesInp");
+const getBtn = document.getElementById("getBtn");
+
+let allCtrls = {};
+let allScenes = {};
+let allCameras = {};
+let allModels = {};
+let renderer;
+let allRenderers = {};
 
 
-for (let index = 0; index < getBtn.length; index++) {
-    const element = getBtn[index];
-    element.addEventListener("click", render3D);
+getBtn.addEventListener("click", get);
+
+function get() {
+    const fileCount = selectedFiles.files.length;
+    console.log(fileCount);
+    for (let i = 0; i < fileCount; i++) {
+        renderAll(i);
+    }
 }
 
-let renderingDivId;
-function render3D(ev) {
-    renderingDiv = ev.target.closest(".eachDiv");
-    let selectedFileElClass = renderingDiv.id + "Inp";
-    renderingDivId = renderingDiv.id;
-    // console.log(selectedFileElClass);
+function renderAll(i) {
 
-    selectedFileEl = document.querySelector(`.${selectedFileElClass}`);
-    // console.log(div);
-    renderingDiv.innerHTML = "";
-    init();
-}
+    let camera, scene;
+    let hlight, directionalLight;
 
 
-let scene, camera, renderer;
-let hlight, directionalLight;
+    let g = document.createElementNS("http://www.w3.org/2000/svg", 'g');
 
+    var newNode = document.createElementNS("http://www.w3.org/2000/svg", 'foreignObject');
+    let attributesForNewNode = {width:"300px", height:"200px", x:`${350*i}`, y: "0"};
+    setAttributes(newNode, attributesForNewNode);
+    g.appendChild(newNode);
 
-function init() {
+    var renderingDiv = document.createElement('div');
+    var divIdName = `div_${i}`;
+    setAttributes(renderingDiv, {id:divIdName, class:"eachDiv"});
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
-    camera.position.set(-5, 2, -5);
+    var fullViewBtn = document.createElement("input");
+    let fullViewBtnAttributes = {type:"button", value:'↗️', class:"fullScreenBtn", id:"fullViewBtn"};
+    setAttributes(fullViewBtn, fullViewBtnAttributes);
 
-    // let position = new THREE.Vector3(-5, 2, -5);
+    fullViewBtn.addEventListener('click', fullView);
 
+    renderingDiv.appendChild(fullViewBtn);
+    newNode.appendChild(renderingDiv);
+    fullSvgEl.appendChild(g);
 
-    scene = new THREE.Scene();
-    const color = new THREE.Color("skyblue");
-    scene.background = color;
-
-    hlight = new THREE.AmbientLight(0x404040, 100);
-    scene.add(hlight);
-
-    directionalLight = new THREE.DirectionalLight(0xffffff, 10);
-    directionalLight.position.set(0, 1, 0);
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
-
-
-    const loader = new GLTFLoader();
-    const fileSrc = URL.createObjectURL(selectedFileEl.files[0]);
-    // allFiles[renderingDiv.id] = fileSrc;
-    loader.load(fileSrc, async function (gltf) {
-        // console.log("loading....");
-        const model = gltf.scene;
-        allModels.push(model);
-        // model.scale.set(0.01, 0.01, 0.01);
-
-
-
-        // Size setting 
-        var boundingBox = new THREE.Box3().setFromObject(model);
-        // console.log(boundingBox);
-
-        var size = new THREE.Vector3();
-        boundingBox.getSize(size);
-        // console.log(boundingBox.getSize(size));
-        var maxSize = new THREE.Vector3(5, 5, 5);
-        var minSize = new THREE.Vector3(1.3, 1.3, 1.3);
-
-        if (size.x > maxSize.x || size.y > maxSize.y || size.z > maxSize.z) {
-            var scaleFactor = Math.min(maxSize.x / size.x, maxSize.y / size.y, maxSize.z / size.z);
-            model.scale.set(scaleFactor, scaleFactor, scaleFactor);
-        }
-
-        if (size.x < minSize || size.y < minSize.y || size.z < minSize.z) {
-            var scaleFactor = Math.max(minSize.x / size.x, minSize.y / size.y, minSize.z / size.z);
-            model.scale.set(scaleFactor, scaleFactor, scaleFactor);
-        }
-
-
-
-        await renderer.compileAsync(scene, camera);
-        scene.add(model);
-        render();
+    renderingDiv.addEventListener("mouseenter", function () {
+        fullViewBtn.style.display = "block";
     });
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(devicePixelRatio);
-    renderer.setSize(300, 200);
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = 0.5;
+    renderingDiv.addEventListener("mouseleave", function () {
+        fullViewBtn.style.display = "none";
+    })
 
-    renderingDiv.appendChild(renderer.domElement);
-    renderer.domElement.id = `${renderingDiv.id}Cnv`;
-
-    camera.lookAt(new THREE.Vector3());
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    allCtrrls.push(controls);
-
-    // controls.addEventListener('change', render);
-    // controls.minDistance = 0.1;
-    // controls.maxDistance = 900;
-    // controls.target.set(0, 0, 0);
-    // controls.update();
-}
+    innit();
 
 
-
-let allDivs = document.querySelectorAll(".eachDiv");
-
-for (let i = 0; i < allDivs.length; i++) {
-    allDivs[i].addEventListener("click", ctrls);
-}
-
-
-let selectedDiv;
-let divId
-function ctrls(ev) {
-    let cnv;
-    selectedDiv = ev.target.closest(".eachDiv");
-    // console.log(selectedDiv);
-
-    let idxForModel;
-    let idxForcamera;
-    if (selectedDiv) {
-        divId = selectedDiv.id;
-        idxForModel = classToIdx[divId];
-        idxForcamera = classToIdx[divId];
-        if (selectedDiv.childElementCount == 1) {
-            let cnvId = divId + "Cnv";
-            cnv = document.getElementById(cnvId);
-            add();
-        }
-    }
-
-    function add() {
+    function innit() {
         camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
-        let x = cameraData[idxForcamera].x;
-        let y = cameraData[idxForcamera].y;
-        let z = cameraData[idxForcamera].z;
-        camera.position.set(x, y, z);
-        camera.lookAt(new THREE.Vector3());
+        camera.position.set(-5, 2, -5);
 
         scene = new THREE.Scene();
         const color = new THREE.Color("skyblue");
         scene.background = color;
-
-        scene.add(allModels[idxForModel]);
 
         hlight = new THREE.AmbientLight(0x404040, 100);
         scene.add(hlight);
@@ -165,41 +78,161 @@ function ctrls(ev) {
         directionalLight.castShadow = true;
         scene.add(directionalLight);
 
-        renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setPixelRatio(devicePixelRatio);
-        renderer.setSize(300, 200);
-        renderer.toneMapping = THREE.ACESFilmicToneMapping;
-        renderer.toneMappingExposure = 0.5;
+        // console.log(i);
+        const loader = new GLTFLoader();
+        const fileSrc = URL.createObjectURL(selectedFiles.files[i]);
+        // console.log(fileSrc);
 
-        async function name() {
+        loader.load(fileSrc, async function (gltf) {
+            const model = gltf.scene;
+            allModels[i] = model;
+
+            var boundingBox = new THREE.Box3().setFromObject(model);
+            // console.log(boundingBox);
+
+            var size = new THREE.Vector3();
+            boundingBox.getSize(size);
+            // console.log(boundingBox.getSize(size));
+            var maxSize = new THREE.Vector3(5, 5, 5);
+            var minSize = new THREE.Vector3(1.3, 1.3, 1.3);
+
+            if (size.x > maxSize.x || size.y > maxSize.y || size.z > maxSize.z) {
+                var scaleFactor = Math.min(maxSize.x / size.x, maxSize.y / size.y, maxSize.z / size.z);
+                model.scale.set(scaleFactor, scaleFactor, scaleFactor);
+            }
+
+            if (size.x < minSize || size.y < minSize.y || size.z < minSize.z) {
+                var scaleFactor = Math.max(minSize.x / size.x, minSize.y / size.y, minSize.z / size.z);
+                model.scale.set(scaleFactor, scaleFactor, scaleFactor);
+            }
+
+            renderer = new THREE.WebGLRenderer({ antialias: true });
+            renderer.setPixelRatio(devicePixelRatio);
+            renderer.setSize(300, 200);
+            renderer.toneMapping = THREE.ACESFilmicToneMapping;
+            renderer.toneMappingExposure = 0.5;
+
+            camera.lookAt(new THREE.Vector3());
+
             await renderer.compileAsync(scene, camera);
-        }
+            allRenderers[i] = renderer;
 
-        name();
+            scene.add(model);
+            renderer.render(scene, camera);
+            allCameras[i] = camera;
+            allScenes[i] = scene;
 
-        selectedDiv.innerHTML = '';
-        selectedDiv.appendChild(renderer.domElement)
+            renderingDiv.appendChild(renderer.domElement);
+            renderingDiv.addEventListener("click", addCtrls);
 
-        render();
+        });
+    }
 
+}
+
+function addCtrls(ev) {
+    let div = ev.target.closest(".eachDiv");
+    let divId = div.id;
+    let Idx = divId[divId.length - 1];
+    console.log(+Idx);
+
+    let renderer = allRenderers[Idx];
+    let scene = allScenes[Idx];
+    let camera = allCameras[Idx];
+
+
+    if (!allCtrls[Idx]) {
+        console.log("new control added");
         let controls = new OrbitControls(camera, renderer.domElement);
         controls.addEventListener('change', render);
         controls.minDistance = 0.1;
         controls.maxDistance = 900;
         controls.target.set(0, 0, 0);
         controls.update();
+        allCtrls[Idx] = controls;
     }
+
+
+    function render() {
+        renderer.render(scene, camera)
+    }
+
 
 }
 
 
-function render() {
-    renderer.render(scene, camera);
-    if (selectedDiv) {
-        let idxForCamera = cameraData[classToIdx[selectedDiv.id]];
-        idxForCamera.x = camera.position.x;
-        idxForCamera.y = camera.position.y;
-        idxForCamera.z = camera.position.z;
+function fullView(ev) {
+    let div = ev.target.closest(".eachDiv");
+    let divId = div.id;
+    let Idx = divId[divId.length - 1];
+    console.log(Idx);
+    console.log("fulll");
+    let scene = allScenes[+Idx];
+    let camera = allCameras[+Idx];
+    let renderer = allRenderers[+Idx];
+
+    // let controler = allCtrls[Idx];
+
+    makeFullDom();
+    function makeFullDom() {
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+
+        let g = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+
+        var newNode = document.createElementNS("http://www.w3.org/2000/svg", 'foreignObject');
+        let newNodeAttributes = {width: width, height: height, x:"0", y:"0"};
+        setAttributes(newNode, newNodeAttributes);
+
+        g.appendChild(newNode);
+
+        var renderingDiv = document.createElement('div');
+        renderingDiv.setAttribute("class", "fullViewDiv");
+
+        var exitBtn = document.createElement("input");
+        const exitBtnAttributes = {type:"button", value:"↖️", class:"exitBtn"};
+        setAttributes(exitBtn, exitBtnAttributes);
+        exitBtn.addEventListener("click", exit);
+        renderingDiv.appendChild(exitBtn);
+
+        newNode.appendChild(renderingDiv);
+        fullSvgEl.appendChild(g);
+
+        renderer.setSize(width, height);
+        renderer.render(scene, camera);
+        renderingDiv.appendChild(renderer.domElement);
+
+        function exit() {
+            g.remove();
+            renderer.setSize(300, 200);
+            div.innerHTML = "";
+            div.appendChild(renderer.domElement);
+
+            var fullViewBtn = document.createElement("input");
+            const fullViewBtnAttributes = {type:"button", value:"↗️", class:"fullScreenBtn", id:"fullViewBtn"};
+            setAttributes(fullViewBtn, fullViewBtnAttributes);
+            fullViewBtn.addEventListener('click', fullView);
+
+            div.appendChild(fullViewBtn);
+
+            div.addEventListener("mouseenter", function () {
+                fullViewBtn.style.display = "block";
+            });
+            div.addEventListener("mouseleave", function () {
+                fullViewBtn.style.display = "none";
+            })
+
+
+        }
+
     }
-    // console.log("loaded");
+
+}
+
+function setAttributes(element, allAttributes) {
+    let attriButes = Object.keys(allAttributes);
+    let attributeValues = Object.values(allAttributes);
+    for(let i=0; i<attriButes.length; i++){
+        element.setAttribute(attriButes[i], attributeValues[i]);
+    }
 }
